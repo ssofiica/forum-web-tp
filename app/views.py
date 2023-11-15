e0c681ff21a0b18d929c85e3a462656128988f8f
ssofiica <s.valova2016@yandex.ru>
2023-11-15 10:21:29 +0300

    v1

--- app/views.py
+++ app/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+QUEST_NUMBER = 20
+ANSWER_NUMBER = 5
+
+def pagination(objects, request, per_page=10):
+    paginator = Paginator(objects, per_page)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return page_obj
 
 def questions(request):
-    return render(request, 'index.html', {'data': range(5)})
+    questions = []
+    for i in range(1,45):
+        questions.append({'title': 'Title ' + str(i),
+        'id': i, 'text': 'text ' + str(i)
+    })
+    page_obj = pagination(questions, request, QUEST_NUMBER)
+    return render(request, 'index.html', {'data': {'quest': questions, 'page_obj': page_obj}})
+
+def hot(request):
+    return render(request, '')
+
+def tag(request):
+    return render(request, '')
 
 def ask(request):
     return render(request, 'ask.html')
@@ -12,5 +34,16 @@ def login(request):
 def signup(request):
     return render(request, 'signup.html')
 
-def question(request):
-    return render(request, 'question.html', {'data': range(5)})
+def settings(request):
+    user = {'login': 'user-login', 'email': 'user@mail.ru', 'nickname': 'Mr. User'}
+    return render(request, 'settings.html', {'data': user})
+
+def question(request, id):
+    q = {'title': 'Title ' + str(id),
+        'id': id, 'text': 'text ' + str(id)
+    }
+    a = []
+    for i in range(1, 10):
+        a.append({'text': 'text' + str(i)})
+    page_obj = pagination(a, request, ANSWER_NUMBER)
+    return render(request, 'question.html', {'data': {'quest': q, 'answers': a, 'page_obj': page_obj}})
--- forum/urls.py
+++ forum/urls.py
@@ -4,9 +4,12 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('questions/', views.questions, name='list_url'),
-    path('question/', views.question, name='question_url'),
+    path('/', views.questions, name='list_url'),
+    path('hot/', views.hot, name='hot_url'),
+    path('tag/', views.tag, name='tag_url'),
+    path('question/<int:id>/', views.question, name='question_url'),
     path('ask/', views.ask, name='ask_url'),
     path('login/', views.login, name='login_url'),
     path('signup/', views.signup, name='signup_url'),
+    path('settings/', views.settings, name='settings_url'),
 ]
